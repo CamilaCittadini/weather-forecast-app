98bb8cc8f8420a0bce81de7174afa55b27246b89
Camila Cittadini <cittadini.camilao@gmail.com>
2023-01-06 13:46:00 -0300

    fixed url bug

--- src/services/currentAndForecast.ts
+++ src/services/currentAndForecast.ts
@@ -12,7 +12,7 @@ export const getCurrentWeather = async (location: LocationParamsType) => {
 //Api call to retrieve forecast based on location
 export const getForecast = async (id?: number) => {
   return await axios<any, Forecast>({
-    url: `http://api.openweathermap.org/data/2.5/forecast?id=${id}&appid=${process.env.REACT_APP_API_KEY}`,
+    url: `https://api.openweathermap.org/data/2.5/forecast?id=${id}&appid=${process.env.REACT_APP_API_KEY}`,
     method: "GET",
   });
 };
